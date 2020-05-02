63535db764d6121278a2b5b40d37746611e0a0a4
Jan Tojnar <jtojnar@gmail.com>
2020-05-02 05:14:27 +0200

    Fix readme test
    Curl does not allow loading local files by default so I am using MockHandler.
    I think it is still easier than replacing the URL and trying to reconfigure curl.

--- tests/ReadmeTest.php
+++ tests/ReadmeTest.php
@@ -2,10 +2,19 @@
 
 namespace Fossar\GuzzleTranscoder\Tests;
 
+use GuzzleHttp\Handler\MockHandler;
+use GuzzleHttp\Psr7\Response;
+
 class ReadmeTest extends \PHPUnit\Framework\TestCase {
     public function testReadme() {
         $contents = file_get_contents(__DIR__ . '/../README.md');
-        str_replace('http://www.myseosolution.de/scripts/encoding-test.php?enc=iso', 'tests/resources/iso-8859-1.html', $contents);
+
+        $mock = new MockHandler([
+            new Response(200, ['content-type' => 'text/html; charset=iso-8859-1; someOtherRandom="header in here"'], file_get_contents(__DIR__ . '/resources/iso-8859-1.html')),
+        ]);
+
+        $contents = str_replace('HandlerStack::create();', 'HandlerStack::create($mock);', $contents);
+
         preg_match_all('(```php\n(.*?)\n```)s', $contents, $matches, PREG_SET_ORDER);
 
         $codes = array_map(function($match) {
