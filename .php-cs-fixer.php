4386d7f9b889ead535827f976a110d135883a4cc
Jan Tojnar <jtojnar@gmail.com>
2026-07-22 06:54:32 +0200

    php-cs-fixer: Format

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 $finder = PhpCsFixer\Finder::create()
     ->in(__DIR__);
 
@@ -36,6 +38,6 @@ $config = new PhpCsFixer\Config();
 
 return $config
     ->setRules($rules)
-    ->setIndent("    ")
+    ->setIndent('    ')
     ->setRiskyAllowed(true)
     ->setFinder($finder);
